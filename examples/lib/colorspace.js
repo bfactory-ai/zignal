2d81e6a5c0f62ac0c2c12ce93ccc3477b35fab72
Adrià Arrufat <adria.arrufat@gmail.com>
2024-10-15 12:05:17 +0900

    examples: update colorspace converter

--- examples/lib/colorspace.js
+++ examples/lib/colorspace.js
@@ -48,61 +48,68 @@
     wasm_exports = obj.instance.exports;
     window.wasm = obj;
     // Event listeners for HEX
-    document.getElementById('hex-str').addEventListener('input', updateHex);
+    document.getElementById('hex-#').addEventListener('input', updateFromHex);
     // Event listeners for RGB
-    document.getElementById('rgb-r').addEventListener('input', updateRgb);
-    document.getElementById('rgb-g').addEventListener('input', updateRgb);
-    document.getElementById('rgb-b').addEventListener('input', updateRgb);
+    document.getElementById('rgb-r').addEventListener('input', updateFromRgb);
+    document.getElementById('rgb-g').addEventListener('input', updateFromRgb);
+    document.getElementById('rgb-b').addEventListener('input', updateFromRgb);
     // Event listeners for HSL
-    document.getElementById('hsl-h').addEventListener('input', updateHsl);
-    document.getElementById('hsl-s').addEventListener('input', updateHsl);
-    document.getElementById('hsl-l').addEventListener('input', updateHsl);
+    document.getElementById('hsl-h').addEventListener('input', updateFromHsl);
+    document.getElementById('hsl-s').addEventListener('input', updateFromHsl);
+    document.getElementById('hsl-l').addEventListener('input', updateFromHsl);
     // Event listeners for HSV
-    document.getElementById('hsv-h').addEventListener('input', updateHsv);
-    document.getElementById('hsv-s').addEventListener('input', updateHsv);
-    document.getElementById('hsv-v').addEventListener('input', updateHsv);
+    document.getElementById('hsv-h').addEventListener('input', updateFromHsv);
+    document.getElementById('hsv-s').addEventListener('input', updateFromHsv);
+    document.getElementById('hsv-v').addEventListener('input', updateFromHsv);
     // Event listeners for XYZ
-    document.getElementById('xyz-x').addEventListener('input', updateXyz);
-    document.getElementById('xyz-y').addEventListener('input', updateXyz);
-    document.getElementById('xyz-z').addEventListener('input', updateXyz);
+    document.getElementById('xyz-x').addEventListener('input', updateFromXyz);
+    document.getElementById('xyz-y').addEventListener('input', updateFromXyz);
+    document.getElementById('xyz-z').addEventListener('input', updateFromXyz);
     // Event listeners for LAB
-    document.getElementById('lab-l').addEventListener('input', updateLab);
-    document.getElementById('lab-a').addEventListener('input', updateLab);
-    document.getElementById('lab-b').addEventListener('input', updateLab);
+    document.getElementById('lab-l').addEventListener('input', updateFromLab);
+    document.getElementById('lab-a').addEventListener('input', updateFromLab);
+    document.getElementById('lab-b').addEventListener('input', updateFromLab);
 
-    function updateHex() {
-      const hex = document.getElementById('hex-str').value;
+    function updateFromHex() {
+      const hex = document.getElementById('hex-#').value;
       const rgb = hex2rgb(hex);
       if (rgb) {
         document.getElementById('rgb-r').value = rgb.r;
         document.getElementById('rgb-g').value = rgb.g;
         document.getElementById('rgb-b').value = rgb.b;
-        updateRgb();
+        updateFromRgb();
       }
+    }
+
+    function updateHex() {
+      document.getElementById('rgb-r').value = rgb.r;
+      document.getElementById('rgb-g').value = rgb.g;
+      document.getElementById('rgb-b').value = rgb.b;
+      document.getElementById('hex-#').value = rgb2hex(r, g, b);
     }
 
     // --- RGB ---
     function rgb2hsl(r, g, b) {
       const hslPtr = wasm_exports.alloc(3 * 4);
-      const hsl = new Float32Array(wasm_exports.memory.buffer, hslPtr, 3);
+      const hsl = new Float64Array(wasm_exports.memory.buffer, hslPtr, 3);
       wasm_exports.rgb2hsl(r, g, b, hslPtr);
-      document.getElementById('hsl-h').value = Math.round(hsl[0]);
-      document.getElementById('hsl-s').value = Math.round(hsl[1]);
-      document.getElementById('hsl-l').value = Math.round(hsl[2]);
+      document.getElementById('hsl-h').value = hsl[0];
+      document.getElementById('hsl-s').value = hsl[1];
+      document.getElementById('hsl-l').value = hsl[2];
     }
 
     function rgb2hsv(r, g, b) {
       const hsvPtr = wasm_exports.alloc(3 * 4);
-      const hsv = new Float32Array(wasm_exports.memory.buffer, hsvPtr, 3);
+      const hsv = new Float64Array(wasm_exports.memory.buffer, hsvPtr, 3);
       wasm_exports.rgb2hsv(r, g, b, hsvPtr);
-      document.getElementById('hsv-h').value = Math.round(hsv[0]);
-      document.getElementById('hsv-s').value = Math.round(hsv[1]);
-      document.getElementById('hsv-v').value = Math.round(hsv[2]);
+      document.getElementById('hsv-h').value = hsv[0];
+      document.getElementById('hsv-s').value = hsv[1];
+      document.getElementById('hsv-v').value = hsv[2];
     }
 
     function rgb2xyz(r, g, b) {
       const xyzPtr = wasm_exports.alloc(3 * 4);
-      const xyz = new Float32Array(wasm_exports.memory.buffer, xyzPtr, 3);
+      const xyz = new Float64Array(wasm_exports.memory.buffer, xyzPtr, 3);
       wasm_exports.rgb2xyz(r, g, b, xyzPtr);
       document.getElementById('xyz-x').value = xyz[0];
       document.getElementById('xyz-y').value = xyz[1];
@@ -111,14 +118,14 @@
 
     function rgb2lab(r, g, b) {
       const labPtr = wasm_exports.alloc(3 * 4);
-      const lab = new Float32Array(wasm_exports.memory.buffer, labPtr, 3);
+      const lab = new Float64Array(wasm_exports.memory.buffer, labPtr, 3);
       wasm_exports.rgb2lab(r, g, b, labPtr);
       document.getElementById('lab-l').value = lab[0];
       document.getElementById('lab-a').value = lab[1];
       document.getElementById('lab-b').value = lab[2];
     }
 
-    function updateRgb() {
+    function updateFromRgb() {
       const r = parseInt(document.getElementById('rgb-r').value);
       const g = parseInt(document.getElementById('rgb-g').value);
       const b = parseInt(document.getElementById('rgb-b').value);
@@ -126,7 +133,7 @@
       rgb2hsv(r, g, b);
       rgb2xyz(r, g, b);
       rgb2lab(r, g, b);
-      document.getElementById('hex-str').value = rgb2hex(r, g, b);
+      document.getElementById('hex-#').value = rgb2hex(r, g, b);
     }
 
     // --- HSL ---
@@ -142,16 +149,16 @@
 
     function hsl2hsv(h, s, l) {
       const hsvPtr = wasm_exports.alloc(3 * 4);
-      const hsv = new Float32Array(wasm_exports.memory.buffer, hsvPtr, 3);
+      const hsv = new Float64Array(wasm_exports.memory.buffer, hsvPtr, 3);
       wasm_exports.hsl2hsv(h, s, l, hsvPtr);
-      document.getElementById('hsv-h').value = Math.round(hsv[0]);
-      document.getElementById('hsv-s').value = Math.round(hsv[1]);
-      document.getElementById('hsv-v').value = Math.round(hsv[2]);
+      document.getElementById('hsv-h').value = hsv[0];
+      document.getElementById('hsv-s').value = hsv[1];
+      document.getElementById('hsv-v').value = hsv[2];
     }
 
     function hsl2xyz(h, s, l) {
       const xyzPtr = wasm_exports.alloc(3 * 4);
-      const xyz = new Float32Array(wasm_exports.memory.buffer, xyzPtr, 3);
+      const xyz = new Float64Array(wasm_exports.memory.buffer, xyzPtr, 3);
       wasm_exports.hsl2xyz(h, s, l, xyzPtr);
       document.getElementById('xyz-x').value = xyz[0];
       document.getElementById('xyz-y').value = xyz[1];
@@ -160,14 +167,14 @@
 
     function hsl2lab(h, s, l) {
       const labPtr = wasm_exports.alloc(3 * 4);
-      const lab = new Float32Array(wasm_exports.memory.buffer, labPtr, 3);
+      const lab = new Float64Array(wasm_exports.memory.buffer, labPtr, 3);
       wasm_exports.hsl2lab(h, s, l, labPtr);
       document.getElementById('lab-l').value = lab[0];
       document.getElementById('lab-a').value = lab[1];
       document.getElementById('lab-b').value = lab[2];
     }
 
-    function updateHsl() {
+    function updateFromHsl() {
       const h = parseFloat(document.getElementById('hsl-h').value);
       const s = parseFloat(document.getElementById('hsl-s').value);
       const l = parseFloat(document.getElementById('hsl-l').value);
@@ -178,7 +185,7 @@
       const red = parseInt(document.getElementById('rgb-r').value);
       const green = parseInt(document.getElementById('rgb-g').value);
       const blue = parseInt(document.getElementById('rgb-b').value);
-      document.getElementById('hex-str').value = rgb2hex(red, green, blue);
+      document.getElementById('hex-#').value = rgb2hex(red, green, blue);
     }
 
     // --- HSV ---
@@ -194,16 +201,16 @@
 
     function hsv2hsl(h, s, v) {
       const hslPtr = wasm_exports.alloc(3 * 4)
-      const hsl = new Float32Array(wasm_exports.memory.buffer, hslPtr, 3);
+      const hsl = new Float64Array(wasm_exports.memory.buffer, hslPtr, 3);
       wasm_exports.hsv2hsl(h, s, v, hslPtr);
-      document.getElementById('hsl-h').value = Math.round(hsl[0]);
-      document.getElementById('hsl-s').value = Math.round(hsl[1]);
-      document.getElementById('hsl-l').value = Math.round(hsl[2]);
+      document.getElementById('hsl-h').value = hsl[0];
+      document.getElementById('hsl-s').value = hsl[1];
+      document.getElementById('hsl-l').value = hsl[2];
     }
 
     function hsv2xyz(h, s, v) {
       const xyzPtr = wasm_exports.alloc(3 * 4)
-      const xyz = new Float32Array(wasm_exports.memory.buffer, xyzPtr, 3);
+      const xyz = new Float64Array(wasm_exports.memory.buffer, xyzPtr, 3);
       wasm_exports.hsv2xyz(h, s, v, xyzPtr);
       document.getElementById('xyz-x').value = xyz[0];
       document.getElementById('xyz-y').value = xyz[1];
@@ -212,14 +219,14 @@
 
     function hsv2lab(h, s, v) {
       const labPtr = wasm_exports.alloc(3 * 4)
-      const lab = new Float32Array(wasm_exports.memory.buffer, labPtr, 3);
+      const lab = new Float64Array(wasm_exports.memory.buffer, labPtr, 3);
       wasm_exports.hsv2lab(h, s, v, labPtr);
       document.getElementById('lab-l').value = lab[0];
       document.getElementById('lab-a').value = lab[1];
       document.getElementById('lab-b').value = lab[2];
     }
 
-    function updateHsv() {
+    function updateFromHsv() {
       const h = parseFloat(document.getElementById('hsv-h').value);
       const s = parseFloat(document.getElementById('hsv-s').value);
       const v = parseFloat(document.getElementById('hsv-v').value);
@@ -227,10 +234,7 @@
       hsv2hsl(h, s, v);
       hsv2xyz(h, s, v);
       hsv2lab(h, s, v);
-      const red = parseInt(document.getElementById('rgb-r').value);
-      const green = parseInt(document.getElementById('rgb-g').value);
-      const blue = parseInt(document.getElementById('rgb-b').value);
-      document.getElementById('hex-str').value = rgb2hex(red, green, blue);
+      updateHex();
     }
 
     // --- XYZ ---
@@ -246,32 +250,32 @@
 
     function xyz2hsl(x, y, z) {
       const hslPtr = wasm_exports.alloc(3 * 4)
-      const hsl = new Float32Array(wasm_exports.memory.buffer, hslPtr, 3);
+      const hsl = new Float64Array(wasm_exports.memory.buffer, hslPtr, 3);
       wasm_exports.xyz2hsl(x, y, z, hslPtr);
-      document.getElementById('hsl-h').value = Math.round(hsl[0]);
-      document.getElementById('hsl-s').value = Math.round(hsl[1]);
-      document.getElementById('hsl-l').value = Math.round(hsl[2]);
+      document.getElementById('hsl-h').value = hsl[0];
+      document.getElementById('hsl-s').value = hsl[1];
+      document.getElementById('hsl-l').value = hsl[2];
     }
 
     function xyz2hsv(x, y, z) {
       const hsvPtr = wasm_exports.alloc(3 * 4)
-      const hsv = new Float32Array(wasm_exports.memory.buffer, hsvPtr, 3);
+      const hsv = new Float64Array(wasm_exports.memory.buffer, hsvPtr, 3);
       wasm_exports.xyz2hsv(x, y, z, hsvPtr);
-      document.getElementById('hsl-h').value = Math.round(hsv[0]);
-      document.getElementById('hsl-s').value = Math.round(hsv[1]);
-      document.getElementById('hsl-l').value = Math.round(hsv[2]);
+      document.getElementById('hsl-h').value = hsv[0];
+      document.getElementById('hsl-s').value = hsv[1];
+      document.getElementById('hsl-l').value = hsv[2];
     }
 
     function xyz2lab(x, y, z) {
       const labPtr = wasm_exports.alloc(3 * 4)
-      const lab = new Float32Array(wasm_exports.memory.buffer, labPtr, 3);
+      const lab = new Float64Array(wasm_exports.memory.buffer, labPtr, 3);
       wasm_exports.xyz2lab(x, y, z, labPtr);
       document.getElementById('lab-l').value = lab[0];
       document.getElementById('lab-a').value = lab[1];
       document.getElementById('lab-b').value = lab[2];
     }
 
-    function updateXyz() {
+    function updateFromXyz() {
       const x = parseFloat(document.getElementById('xyz-x').value);
       const y = parseFloat(document.getElementById('xyz-y').value);
       const z = parseFloat(document.getElementById('xyz-z').value);
@@ -279,10 +283,7 @@
       xyz2hsl(x, y, z);
       xyz2hsv(x, y, z);
       xyz2lab(x, y, z);
-      const red = parseInt(document.getElementById('rgb-r').value);
-      const green = parseInt(document.getElementById('rgb-g').value);
-      const blue = parseInt(document.getElementById('rgb-b').value);
-      document.getElementById('hex-str').value = rgb2hex(red, green, blue);
+      updateHex();
     }
 
     // --- LAB ---
@@ -298,32 +299,32 @@
 
     function lab2hsl(l, a, b) {
       const hslPtr = wasm_exports.alloc(3 * 4)
-      const hsl = new Float32Array(wasm_exports.memory.buffer, hslPtr, 3);
+      const hsl = new Float64Array(wasm_exports.memory.buffer, hslPtr, 3);
       wasm_exports.lab2hsl(l, a, b, hslPtr);
-      document.getElementById('hsl-h').value = Math.round(hsl[0]);
-      document.getElementById('hsl-s').value = Math.round(hsl[1]);
-      document.getElementById('hsl-l').value = Math.round(hsl[2]);
+      document.getElementById('hsl-h').value = hsl[0];
+      document.getElementById('hsl-s').value = hsl[1];
+      document.getElementById('hsl-l').value = hsl[2];
     }
 
     function lab2hsv(l, a, b) {
       const hsvPtr = wasm_exports.alloc(3 * 4)
-      const hsv = new Float32Array(wasm_exports.memory.buffer, hsvPtr, 3);
+      const hsv = new Float64Array(wasm_exports.memory.buffer, hsvPtr, 3);
       wasm_exports.lab2hsv(l, a, b, hsvPtr);
-      document.getElementById('hsl-h').value = Math.round(hsv[0]);
-      document.getElementById('hsl-s').value = Math.round(hsv[1]);
-      document.getElementById('hsl-l').value = Math.round(hsv[2]);
+      document.getElementById('hsl-h').value = hsv[0];
+      document.getElementById('hsl-s').value = hsv[1];
+      document.getElementById('hsl-l').value = hsv[2];
     }
 
     function lab2xyz(l, a, b) {
       const xyzPtr = wasm_exports.alloc(3 * 4)
-      const xyz = new Float32Array(wasm_exports.memory.buffer, xyzPtr, 3);
+      const xyz = new Float64Array(wasm_exports.memory.buffer, xyzPtr, 3);
       wasm_exports.lab2xyz(l, a, b, xyzPtr);
       document.getElementById('lab-l').value = xyz[0];
       document.getElementById('lab-a').value = xyz[1];
       document.getElementById('lab-b').value = xyz[2];
     }
 
-    function updateLab() {
+    function updateFromLab() {
       const l = parseFloat(document.getElementById('lab-l').value);
       const a = parseFloat(document.getElementById('lab-a').value);
       const b = parseFloat(document.getElementById('lab-b').value);
@@ -331,10 +332,7 @@
       lab2hsl(l, a, b);
       lab2hsv(l, a, b);
       lab2xyz(l, a, b);
-      const red = parseInt(document.getElementById('rgb-r').value);
-      const green = parseInt(document.getElementById('rgb-g').value);
-      const blue = parseInt(document.getElementById('rgb-b').value);
-      document.getElementById('hex-str').value = rgb2hex(red, green, blue);
+      updateHex();
     }
 
   });
--- examples/src/colorspace.zig
+++ examples/src/colorspace.zig
@@ -15,7 +15,7 @@ const Lab = @import("zignal").Lab;
 
 // --- RGB ---
 
-export fn rgb2hsl(red: u8, green: u8, blue: u8, out: [*]f32) void {
+export fn rgb2hsl(red: u8, green: u8, blue: u8, out: [*]f64) void {
     const res = convert(Hsl, Rgb{ .r = red, .g = green, .b = blue });
     std.log.debug("HSL: {[h]d} {[s]d} {[l]d}", res);
     out[0] = res.h;
@@ -23,21 +23,21 @@ export fn rgb2hsl(red: u8, green: u8, blue: u8, out: [*]f32) void {
     out[2] = res.l;
 }
 
-export fn rgb2hsv(red: u8, green: u8, blue: u8, out: [*]f32) void {
+export fn rgb2hsv(red: u8, green: u8, blue: u8, out: [*]f64) void {
     const res = convert(Hsv, Rgb{ .r = red, .g = green, .b = blue });
     std.log.debug("HSV: {[h]d} {[s]d} {[v]d}", res);
     out[0] = res.h;
     out[1] = res.s;
     out[2] = res.v;
 }
-export fn rgb2xyz(red: u8, green: u8, blue: u8, out: [*]f32) void {
+export fn rgb2xyz(red: u8, green: u8, blue: u8, out: [*]f64) void {
     const res = convert(Xyz, Rgb{ .r = red, .g = green, .b = blue });
     std.log.debug("XYZ: {[x]d} {[y]d} {[z]d}", res);
     out[0] = @floatCast(res.x);
     out[1] = @floatCast(res.y);
     out[2] = @floatCast(res.z);
 }
-export fn rgb2lab(red: u8, green: u8, blue: u8, out: [*]f32) void {
+export fn rgb2lab(red: u8, green: u8, blue: u8, out: [*]f64) void {
     const res = convert(Lab, Rgb{ .r = red, .g = green, .b = blue });
     std.log.debug("LAB: {[l]d} {[a]d} {[b]d}", res);
     out[0] = @floatCast(res.l);
@@ -47,7 +47,7 @@ export fn rgb2lab(red: u8, green: u8, blue: u8, out: [*]f32) void {
 
 // --- HSL ---
 
-export fn hsl2rgb(h: f32, s: f32, l: f32, out: [*]u8) void {
+export fn hsl2rgb(h: f64, s: f64, l: f64, out: [*]u8) void {
     const res = convert(Rgb, Hsl{ .h = h, .s = s, .l = l });
     std.log.debug("RGB: {[r]d} {[g]d} {[b]d}", res);
     out[0] = res.r;
@@ -55,7 +55,7 @@ export fn hsl2rgb(h: f32, s: f32, l: f32, out: [*]u8) void {
     out[2] = res.b;
 }
 
-export fn hsl2hsv(h: f32, s: f32, l: f32, out: [*]f32) void {
+export fn hsl2hsv(h: f64, s: f64, l: f64, out: [*]f64) void {
     const res = convert(Hsv, Hsl{ .h = h, .s = s, .l = l });
     std.log.debug("HSV: {[h]d} {[s]d} {[v]d}", res);
     out[0] = res.h;
@@ -63,7 +63,7 @@ export fn hsl2hsv(h: f32, s: f32, l: f32, out: [*]f32) void {
     out[2] = res.v;
 }
 
-export fn hsl2xyz(h: f32, s: f32, l: f32, out: [*]f32) void {
+export fn hsl2xyz(h: f64, s: f64, l: f64, out: [*]f64) void {
     const res = convert(Xyz, Hsl{ .h = h, .s = s, .l = l });
     std.log.debug("XYZ: {[x]d} {[y]d} {[z]d}", res);
     out[0] = @floatCast(res.x);
@@ -71,7 +71,7 @@ export fn hsl2xyz(h: f32, s: f32, l: f32, out: [*]f32) void {
     out[2] = @floatCast(res.z);
 }
 
-export fn hsl2lab(h: f32, s: f32, l: f32, out: [*]f32) void {
+export fn hsl2lab(h: f64, s: f64, l: f64, out: [*]f64) void {
     const res = convert(Lab, Hsl{ .h = h, .s = s, .l = l });
     std.log.debug("LAB: {[l]d} {[a]d} {[b]d}", res);
     out[0] = @floatCast(res.l);
@@ -81,7 +81,7 @@ export fn hsl2lab(h: f32, s: f32, l: f32, out: [*]f32) void {
 
 // --- HSV ---
 
-export fn hsv2rgb(h: f32, s: f32, v: f32, out: [*]u8) void {
+export fn hsv2rgb(h: f64, s: f64, v: f64, out: [*]u8) void {
     const res = convert(Rgb, Hsv{ .h = h, .s = s, .v = v });
     std.log.debug("RGB: {[r]d} {[g]d} {[b]d}", res);
     out[0] = res.r;
@@ -89,7 +89,7 @@ export fn hsv2rgb(h: f32, s: f32, v: f32, out: [*]u8) void {
     out[2] = res.b;
 }
 
-export fn hsv2hsl(h: f32, s: f32, v: f32, out: [*]f32) void {
+export fn hsv2hsl(h: f64, s: f64, v: f64, out: [*]f64) void {
     const res = convert(Hsl, Hsv{ .h = h, .s = s, .v = v });
     std.log.debug("HSL: {[h]d} {[s]d} {[l]d}", res);
     out[0] = res.h;
@@ -97,7 +97,7 @@ export fn hsv2hsl(h: f32, s: f32, v: f32, out: [*]f32) void {
     out[2] = res.l;
 }
 
-export fn hsv2xyz(h: f32, s: f32, v: f32, out: [*]f32) void {
+export fn hsv2xyz(h: f64, s: f64, v: f64, out: [*]f64) void {
     const res = convert(Xyz, Hsv{ .h = h, .s = s, .v = v });
     std.log.debug("XYZ: {[x]d} {[y]d} {[z]d}", res);
     out[0] = @floatCast(res.x);
@@ -105,7 +105,7 @@ export fn hsv2xyz(h: f32, s: f32, v: f32, out: [*]f32) void {
     out[2] = @floatCast(res.z);
 }
 
-export fn hsv2lab(h: f32, s: f32, v: f32, out: [*]f32) void {
+export fn hsv2lab(h: f64, s: f64, v: f64, out: [*]f64) void {
     const res = convert(Lab, Hsv{ .h = h, .s = s, .v = v });
     std.log.debug("LAB: {[l]d} {[a]d} {[b]d}", res);
     out[0] = @floatCast(res.l);
@@ -115,7 +115,7 @@ export fn hsv2lab(h: f32, s: f32, v: f32, out: [*]f32) void {
 
 // --- XYZ ---
 
-export fn xyz2rgb(x: f32, y: f32, z: f32, out: [*]u8) void {
+export fn xyz2rgb(x: f64, y: f64, z: f64, out: [*]u8) void {
     const res = convert(Rgb, Xyz{ .x = x, .y = y, .z = z });
     std.log.debug("RGB: {[r]d} {[g]d} {[b]d}", res);
     out[0] = res.r;
@@ -123,7 +123,7 @@ export fn xyz2rgb(x: f32, y: f32, z: f32, out: [*]u8) void {
     out[2] = res.b;
 }
 
-export fn xyz2hsl(x: f32, y: f32, z: f32, out: [*]f32) void {
+export fn xyz2hsl(x: f64, y: f64, z: f64, out: [*]f64) void {
     const res = convert(Hsl, Xyz{ .x = x, .y = y, .z = z });
     std.log.debug("HSL: {[h]d} {[s]d} {[l]d}", res);
     out[0] = res.h;
@@ -131,7 +131,7 @@ export fn xyz2hsl(x: f32, y: f32, z: f32, out: [*]f32) void {
     out[2] = res.l;
 }
 
-export fn xyz2hsv(x: f32, y: f32, z: f32, out: [*]f32) void {
+export fn xyz2hsv(x: f64, y: f64, z: f64, out: [*]f64) void {
     const res = convert(Hsv, Xyz{ .x = x, .y = y, .z = z });
     std.log.debug("HSV: {[h]d} {[s]d} {[v]d}", res);
     out[0] = res.h;
@@ -139,7 +139,7 @@ export fn xyz2hsv(x: f32, y: f32, z: f32, out: [*]f32) void {
     out[2] = res.v;
 }
 
-export fn xyz2lab(x: f32, y: f32, z: f32, out: [*]f32) void {
+export fn xyz2lab(x: f64, y: f64, z: f64, out: [*]f64) void {
     const res = convert(Lab, Xyz{ .x = x, .y = y, .z = z });
     std.log.debug("LAB: {[l]d} {[a]d} {[b]d}", res);
     out[0] = @floatCast(res.l);
@@ -149,7 +149,7 @@ export fn xyz2lab(x: f32, y: f32, z: f32, out: [*]f32) void {
 
 // --- LAB ---
 
-export fn lab2rgb(l: f32, a: f32, b: f32, out: [*]u8) void {
+export fn lab2rgb(l: f64, a: f64, b: f64, out: [*]u8) void {
     const res = convert(Rgb, Lab{ .l = l, .a = a, .b = b });
     std.log.debug("RGB: {[r]d} {[g]d} {[b]d}", res);
     out[0] = res.r;
@@ -157,7 +157,7 @@ export fn lab2rgb(l: f32, a: f32, b: f32, out: [*]u8) void {
     out[2] = res.b;
 }
 
-export fn lab2hsl(l: f32, a: f32, b: f32, out: [*]f32) void {
+export fn lab2hsl(l: f64, a: f64, b: f64, out: [*]f64) void {
     const res = convert(Hsl, Lab{ .l = l, .a = a, .b = b });
     std.log.debug("HSL: {[h]d} {[s]d} {[l]d}", res);
     out[0] = res.h;
@@ -165,7 +165,7 @@ export fn lab2hsl(l: f32, a: f32, b: f32, out: [*]f32) void {
     out[2] = res.l;
 }
 
-export fn lab2hsv(l: f32, a: f32, b: f32, out: [*]f32) void {
+export fn lab2hsv(l: f64, a: f64, b: f64, out: [*]f64) void {
     const res = convert(Hsv, Lab{ .l = l, .a = a, .b = b });
     std.log.debug("HSV: {[h]d} {[s]d} {[v]d}", res);
     out[0] = res.h;
@@ -173,7 +173,7 @@ export fn lab2hsv(l: f32, a: f32, b: f32, out: [*]f32) void {
     out[2] = res.v;
 }
 
-export fn lab2xyz(l: f32, a: f32, b: f32, out: [*]f32) void {
+export fn lab2xyz(l: f64, a: f64, b: f64, out: [*]f64) void {
     const res = convert(Xyz, Lab{ .l = l, .a = a, .b = b });
     std.log.debug("XYZ: {[x]d} {[y]d} {[z]d}", res);
     out[0] = @floatCast(res.x);
