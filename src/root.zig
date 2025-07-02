3bff7771f1feec6d4f955fc1a76e97a33f0f8e39
Adrià Arrufat <adria.arrufat@gmail.com>
2025-07-02 15:43:34 +0900

    feat(root): export DrawMode type
    Reorganize module exports in root.zig to group related types.
    Move point module import to be internal.

--- src/root.zig
+++ src/root.zig
@@ -1,6 +1,9 @@
 //! Zignal is an image processing library inspired by [dlib](http://dlib.net).
 //! Source code available on [GitHub](https://github.com/bfactory-ai/zignal).
 
+pub const Canvas = @import("canvas.zig").Canvas;
+pub const DrawMode = @import("canvas.zig").DrawMode;
+
 const color = @import("color.zig");
 pub const convertColor = color.convertColor;
 pub const isColor = color.isColor;
@@ -14,8 +17,6 @@ pub const Lms = color.Lms;
 pub const Oklab = color.Oklab;
 pub const Xyb = color.Xyb;
 
-pub const Canvas = @import("canvas.zig").Canvas;
-pub const point = @import("point.zig");
 const geometry = @import("geometry.zig");
 pub const Rectangle = geometry.Rectangle;
 pub const AffineTransform = geometry.AffineTransform;
@@ -26,10 +27,13 @@ pub const Image = @import("image.zig").Image;
 pub const Matrix = @import("matrix.zig").Matrix;
 pub const meta = @import("meta.zig");
 pub const perlin = @import("perlin.zig");
-pub const Point2d = point.Point2d;
-pub const Point3d = point.Point3d;
-pub const svd = @import("svd.zig").svd;
 
 const png = @import("png.zig");
 pub const savePng = png.savePng;
 pub const loadPng = png.loadPng;
+
+const point = @import("point.zig");
+pub const Point2d = point.Point2d;
+pub const Point3d = point.Point3d;
+
+pub const svd = @import("svd.zig").svd;
