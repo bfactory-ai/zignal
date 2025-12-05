d27a795b91292a904c5cc77c55af6cb54d7eeb9f
Adrià Arrufat <adria.arrufat@gmail.com>
2025-12-05 20:50:03 +0900

    docs(python): add ColorSpace enum documentation
    Adds documentation string for the ColorSpace enum used in Python bindings generation.

---
+++ bindings/python/src/color_space.zig
@@ -0,0 +1,20 @@
+//! ColorSpace enum documentation for Python bindings
+pub const color_space_doc =
+    \\Color spaces supported by zignal's color conversions.
+    \\
+    \\Values:
+    \\- `GRAY`
+    \\- `HSL`
+    \\- `HSV`
+    \\- `LAB`
+    \\- `LCH`
+    \\- `LMS`
+    \\- `OKLAB`
+    \\- `OKLCH`
+    \\- `RGB`
+    \\- `RGBA`
+    \\- `XYB`
+    \\- `XYZ`
+    \\- `YCBCR`
+    ;
+
